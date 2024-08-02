33f6ef4e8318db878fc279c79f8d682e1f3e1882
nexoscreator <112468782+nexoscreator@users.noreply.github.com>
2024-08-02 18:41:59 +0530

    update

--- js/search.js
+++ js/search.js
@@ -1,18 +1,1 @@
-// Function to handle form submission
-function handleFormSubmit(event) {
-  event.preventDefault(); // Prevent default form submission behavior
-  const location = document.getElementById('locationInput').value;
-  fetchWeather(location);
-}
-
-// Event listener for form submission
-document.getElementById('searchForm').addEventListener('submit', handleFormSubmit);
-
-  // Display forecast information
-  //  const forecastDiv = document.getElementById('forecast');
-  //  forecastDiv.innerHTML = `
-  //  <h2>Forecast for ${forecast.date}</h2>
-  //  <p>Max Temperature: ${forecast.day.maxtemp_c}°C (${forecast.day.maxtemp_f}°F)</p>
-  //   <p>Min Temperature: ${forecast.day.mintemp_c}°C (${forecast.day.mintemp_f}°F)</p>
-  //   <p>Condition: ${forecast.day.condition.text}</p>
-  //  `;
+function handleFormSubmit(e){e.preventDefault();const t=document.getElementById("locationInput").value;fetchWeather(t)}document.getElementById("searchForm").addEventListener("submit",handleFormSubmit);
