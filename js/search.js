3c20a8433f268503f95c5136a0da61ef34857635
nexoscreator <112468782+nexoscreator@users.noreply.github.com>
2025-02-22 12:00:45 +0530

    style: lint all

--- js/search.js
+++ js/search.js
@@ -8,11 +8,11 @@ function handleFormSubmit(event) {
 // Event listener for form submission
 document.getElementById('searchForm').addEventListener('submit', handleFormSubmit);
 
-  // Display forecast information
-  //  const forecastDiv = document.getElementById('forecast');
-  //  forecastDiv.innerHTML = `
-  //  <h2>Forecast for ${forecast.date}</h2>
-  //  <p>Max Temperature: ${forecast.day.maxtemp_c}°C (${forecast.day.maxtemp_f}°F)</p>
-  //   <p>Min Temperature: ${forecast.day.mintemp_c}°C (${forecast.day.mintemp_f}°F)</p>
-  //   <p>Condition: ${forecast.day.condition.text}</p>
-  //  `;
+// Display forecast information
+//  const forecastDiv = document.getElementById('forecast');
+//  forecastDiv.innerHTML = `
+//  <h2>Forecast for ${forecast.date}</h2>
+//  <p>Max Temperature: ${forecast.day.maxtemp_c}°C (${forecast.day.maxtemp_f}°F)</p>
+//   <p>Min Temperature: ${forecast.day.mintemp_c}°C (${forecast.day.mintemp_f}°F)</p>
+//   <p>Condition: ${forecast.day.condition.text}</p>
+//  `;
